45c2a5de88881c64fc3f4d6e4ae87596016d865d
lamiita <lamiita0205@gmail.com>
2016-04-12 15:39:21 +0200

    starting the menu

---
+++ Assets/Scripts/menu/Spritee.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Spritee : MonoBehaviour
+{
+
+    private Image background;
+    // Use this for initialization
+    void Start()
+    {
+
+        background = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    public void changeSprite(Sprite newSprite)
+    {
+
+        background.sprite = newSprite;
+    }
+}
---
+++ Assets/sprite.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class sprite : MonoBehaviour {
+
+    private Image background;
+	// Use this for initialization
+	void Start () {
+
+        background = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	public void changeSprite(Sprite newSprite)
+    {
+
+        background.sprite = newSprite;
+	}
+}
